9d51a81140fd33ff7ab35eb8fbeacda54d82e995
Mystified131 <mystifiedthomas@gmail.com>
2019-06-12 08:33:37 -0500

    Features added.

--- Encryption/Form1.Designer.vb
+++ Encryption/Form1.Designer.vb
@@ -25,6 +25,12 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.btnEncrypt = New System.Windows.Forms.Button()
         Me.btnDecrypt = New System.Windows.Forms.Button()
+        Me.clrButton = New System.Windows.Forms.Button()
+        Me.dupButton = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.dlgSave = New System.Windows.Forms.SaveFileDialog()
+        Me.dlgOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.btnOpen = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -53,11 +59,55 @@ Partial Class Form1
         Me.btnDecrypt.Text = "Decrypt"
         Me.btnDecrypt.UseVisualStyleBackColor = True
         '
+        'clrButton
+        '
+        Me.clrButton.Location = New System.Drawing.Point(624, 329)
+        Me.clrButton.Name = "clrButton"
+        Me.clrButton.Size = New System.Drawing.Size(89, 42)
+        Me.clrButton.TabIndex = 3
+        Me.clrButton.Text = "Clear"
+        Me.clrButton.UseVisualStyleBackColor = True
+        '
+        'dupButton
+        '
+        Me.dupButton.Location = New System.Drawing.Point(61, 87)
+        Me.dupButton.Name = "dupButton"
+        Me.dupButton.Size = New System.Drawing.Size(116, 55)
+        Me.dupButton.TabIndex = 4
+        Me.dupButton.Text = "Duplicate"
+        Me.dupButton.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(624, 134)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(110, 64)
+        Me.btnSave.TabIndex = 5
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'dlgOpen
+        '
+        Me.dlgOpen.FileName = "dlgOpen"
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(624, 46)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(110, 57)
+        Me.btnOpen.TabIndex = 6
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.dupButton)
+        Me.Controls.Add(Me.clrButton)
         Me.Controls.Add(Me.btnDecrypt)
         Me.Controls.Add(Me.btnEncrypt)
         Me.Controls.Add(Me.TextBox1)
@@ -71,4 +121,10 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents btnEncrypt As Button
     Friend WithEvents btnDecrypt As Button
+    Friend WithEvents clrButton As Button
+    Friend WithEvents dupButton As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents dlgSave As SaveFileDialog
+    Friend WithEvents dlgOpen As OpenFileDialog
+    Friend WithEvents btnOpen As Button
 End Class
--- Encryption/Form1.vb
+++ Encryption/Form1.vb
@@ -8,6 +8,8 @@ Public Class Form1
     Private encryptor As ICryptoTransform
     Private decryptor As ICryptoTransform
 
+    Dim filename As String
+
     Private Sub btnEncrypt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEncrypt.Click
         Dim sPlainText As String = Me.TextBox1.Text
         If Not String.IsNullOrEmpty(sPlainText) Then
@@ -43,4 +45,45 @@ Public Class Form1
         Me.decryptor = symmetricKey.CreateDecryptor(KEY_128, IV_128)
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles clrButton.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub dupButton_Click(sender As Object, e As EventArgs) Handles dupButton.Click
+        Dim astr As String
+        Dim bstr As String
+        astr = TextBox1.Text
+        bstr = astr + astr
+        TextBox1.Text = bstr
+    End Sub
+
+    Private Sub Save(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim dr As DialogResult
+        dlgSave.FileName = filename
+        dr = dlgSave.ShowDialog()
+        If dr = DialogResult.OK Then
+            Try
+                Dim writerVar As StreamWriter
+                writerVar = New StreamWriter(filename, False)
+                writerVar.WriteLine(TextBox1.Text)
+                writerVar.Close()
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim readerVar As StreamReader
+        Dim dr As DialogResult
+        dr = dlgOpen.ShowDialog()
+        If dr = DialogResult.OK Then
+            filename = dlgOpen.FileName
+            readerVar = New StreamReader(dlgOpen.FileName)
+            TextBox1.Text = readerVar.ReadToEnd()
+            readerVar.Close()
+            btnSave.Enabled = True
+            btnOpen.Enabled = False
+        End If
+    End Sub
 End Class
